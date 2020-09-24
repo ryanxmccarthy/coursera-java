c35c8832f61a1faea762a0793871afe4ab57dead
Ryan XVX <ryanxmccarthy@gmail.com>
2020-09-24 14:46:50 -0700

    working on it

--- Week2/GeneralizedHarmonic.java
+++ Week2/GeneralizedHarmonic.java
@@ -1,5 +1,11 @@
 public class GeneralizedHarmonic {
     public static void main(String[] args) {
+        int n = parseInt(args[0]);
+        int r = parseInt(args[1]);
+        double sum = 0;
 
+        for (int i = 0; i < n; i++) {
+            System.out.println(sum);
+        }
     }
 }
